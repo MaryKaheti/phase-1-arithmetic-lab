55aff34ad871df001e188c6fff24863571fdd92f
MaryKaheti <kahetikihara@gmail.com>
2023-03-15 10:21:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+// function multiply(num)
+let (num1) = 31
+let (num2) = 2
+let (num3) = 40
+let (num4) = 9
+
+let multiply = num1 * num2;
+//console.log('num1 * num2');
+let random = Math.floor(Math.random()*10+1);
+
+let mod = num3 % num4;
+let max = Math.max (2,4,6,8,13,17,20)
+
+
